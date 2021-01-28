88b92fee09c0a26afc1abd47ab6056ae4b487b92
ydemokidov <ydemokidov@gmail.com>
2021-01-28 20:58:48 +0300

    lesson 27 JDBCTemplate

--- src/main/java/org/example/SpringMVCLesson/dao/PersonDAO.java
+++ src/main/java/org/example/SpringMVCLesson/dao/PersonDAO.java
@@ -4,6 +4,9 @@ import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.experimental.FieldDefaults;
 import org.example.SpringMVCLesson.model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
 import java.sql.*;
@@ -14,11 +17,17 @@ import java.util.List;
 @Getter
 @Component
 public class PersonDAO {
-    private static final String URL="jdbc:postgresql://localhost:5432/springmvclesson";
-    private static final String username = "postgres";
-    private static final String password = "admin";
+    //private static final String URL="jdbc:postgresql://localhost:5432/springmvclesson";
+    //private static final String username = "postgres";
+    //private static final String password = "admin";
+    private final JdbcTemplate jdbcTemplate;
 
-    private static Connection connection;
+    @Autowired
+    public PersonDAO(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    /*private static Connection connection;
     static{
         try{
             Class.forName("org.postgresql.Driver");
@@ -31,7 +40,7 @@ public class PersonDAO {
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
-    }
+    }*/
 
     //database stub
     static int peopleCount=0;
@@ -46,7 +55,7 @@ public class PersonDAO {
 
     public List<Person> index(){
         //return people;
-        List<Person> people = new ArrayList<>();
+        /*List<Person> people = new ArrayList<>();
 
         try {
             Statement statement = connection.createStatement();
@@ -66,75 +75,25 @@ public class PersonDAO {
         } catch (SQLException throwables) {
             throwables.printStackTrace();
         }
-        return people;
+        return people;*/
+        //return jdbcTemplate.query("SELECT * FROM Person",new PersonRowMapper());
+        return jdbcTemplate.query("SELECT * FROM Person",new BeanPropertyRowMapper<>(Person.class));
     }
 
     public Person show(int id){
-        Person person = null;
-        try {
-            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM Person WHERE ID =?");
-            preparedStatement.setInt(1,id);
-            ResultSet resultSet = preparedStatement.executeQuery();
-            //переходим на первую запись
-            resultSet.next();
-
-            person = new Person();
-            person.setId(resultSet.getInt("id"));
-            person.setName(resultSet.getString("name"));
-            person.setAge(resultSet.getInt("age"));
-            person.setEmail(resultSet.getString("email"));
-
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
-        return person;
+        //return jdbcTemplate.query("SELECT * FROM Person WHERE id=?",new Object[]{id},new PersonRowMapper()).stream().findAny().orElse(null);
+        return jdbcTemplate.query("SELECT * FROM Person WHERE id=?",new Object[]{id},new BeanPropertyRowMapper<>(Person.class)).stream().findAny().orElse(null);
     }
 
     public void save(Person person) {
-        /*person.setId(++peopleCount);
-        people.add(person);*/
-        try {
-            //Statement statement = connection.createStatement();
-            PreparedStatement preparedStatement = connection.prepareStatement("INSERT INTO Person VALUES(?,?,?,?)");
-            preparedStatement.setInt(1,++peopleCount);
-            preparedStatement.setString(2,person.getName());
-            preparedStatement.setInt(3,person.getAge());
-            preparedStatement.setString(4,person.getEmail());
-            //String SQL = "INSERT INTO Person VALUES("+(++peopleCount)+",'"+person.getName()+"',"+person.getAge()+",'"+person.getEmail()+"'";
-            preparedStatement.executeUpdate();
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
+        jdbcTemplate.update("INSERT INTO Person VALUES (?,?,?,?)",++peopleCount,person.getName(),person.getAge(),person.getEmail());
     }
 
     public void update(int id, Person updatePerson){
-        /*Person personToBeUpdated = show(id);
-        personToBeUpdated.setName(updatePerson.getName());
-        personToBeUpdated.setAge(updatePerson.getAge());
-        personToBeUpdated.setEmail(updatePerson.getEmail());*/
-        try {
-            PreparedStatement preparedStatement = connection.prepareStatement("UPDATE Person SET name=?,age=?,email=? WHERE id=?");
-            preparedStatement.setString(1,updatePerson.getName());
-            preparedStatement.setInt(2,updatePerson.getAge());
-            preparedStatement.setString(3,updatePerson.getEmail());
-            preparedStatement.setInt(4,id);
-
-            preparedStatement.executeUpdate();
-
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
-
+        jdbcTemplate.update("UPDATE Person SET name=?, age=?,email=? WHERE id=?",updatePerson.getName(),updatePerson.getAge(),updatePerson.getEmail(),updatePerson.getId());
     }
 
     public void delete(int id) {
-        //people.removeIf(p -> p.getId() == id);
-        try {
-            PreparedStatement preparedStatement = connection.prepareStatement("DELETE FROM Person WHERE id=?");
-            preparedStatement.setInt(1,id);
-            preparedStatement.executeUpdate();
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        }
+        jdbcTemplate.update("DELETE FROM Person WHERE id=?",id);
     }
 }
---
+++ src/main/java/org/example/SpringMVCLesson/dao/PersonRowMapper.java
@@ -0,0 +1,19 @@
+package org.example.SpringMVCLesson.dao;
+
+import org.example.SpringMVCLesson.model.Person;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class PersonRowMapper implements RowMapper<Person> {
+    @Override
+    public Person mapRow(ResultSet resultSet, int i) throws SQLException {
+        Person person = new Person();
+        person.setId(resultSet.getInt("id"));
+        person.setName(resultSet.getString("name"));
+        person.setAge(resultSet.getInt("age"));
+        person.setEmail(resultSet.getString("email"));
+        return person;
+    }
+}
